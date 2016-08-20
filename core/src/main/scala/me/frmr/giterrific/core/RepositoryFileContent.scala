3f71664593cfa294146958278b7eb0a383aa66b7
Matt Farmer <matt@frmr.me>
2016-08-20 10:08:30 -0400

    Start adding some of the basic data structures.

---
+++ core/src/main/scala/me/frmr/giterrific/core/RepositoryFileContent.scala
@@ -0,0 +1,16 @@
+package me.frmr.giterrific.core
+
+/**
+ * A data structure representing the file content of a file within the repository.
+ *
+ * @param sha The sha of the file's blob in the Git database.
+ * @param content The content of the file.
+ * @param encoding The encoding used for the content of the file. Either base64 or utf-8.
+ * @param size The size of the file.
+ */
+case class RepositoryFileContent(
+  sha: String,
+  content: String,
+  encoding: String,
+  size: Long
+)
---
+++ core/src/main/scala/me/frmr/giterrific/core/RepositorySummary.scala
@@ -0,0 +1,14 @@
+package me.frmr.giterrific.core
+
+/**
+ * A data structure providing a brief summary of a repository.
+ *
+ * @param name The short name of the repository.
+ * @param fullName The full path to the repository on the repository server.
+ * @param description The human description of the repository
+ */
+case class RepositorySummary(
+  name: String,
+  fullName: String,
+  description: String
+)
