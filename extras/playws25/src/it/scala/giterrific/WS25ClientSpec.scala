77545af3adf5712bfa3efdf0e1c07177b7d1c52b
Matt Farmer <matt@frmr.me>
2016-08-28 14:21:33 -0400

    Switch to using AhcWSClient for Play 2.5's tests.

--- extras/playws25/src/it/scala/giterrific/WS25ClientSpec.scala
+++ extras/playws25/src/it/scala/giterrific/WS25ClientSpec.scala
@@ -4,12 +4,12 @@ import giterrific.client._
 import giterrific.driver.http._
 import org.scalatest._
 import org.scalatestplus.play.guice._
-import play.api.libs.ws.ning._
+import play.api.libs.ws.ahc._
 
 class WS25ClientSpec extends ClientSpec[WS25HttpReq] with GuiceFakeApplicationFactory with BeforeAndAfterAll {
   val app = fakeApplication()
   implicit lazy val materializer = app.materializer
-  val wsClient = NingWSClient()
+  val wsClient = AhcWSClient()
   val driver = WS25HttpDriver(wsClient)
   override val testClient = new GiterrificClient("http://localhost:8080", driver)
 
