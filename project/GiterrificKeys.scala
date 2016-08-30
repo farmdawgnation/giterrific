678be161f861d21373a3ba3ff7d219db5fc7db16
Matt Farmer <matt@frmr.me>
2016-08-30 00:29:17 -0400

    Bump to version 0.1.0 for release.

--- project/GiterrificKeys.scala
+++ project/GiterrificKeys.scala
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 object GiterrificKeys {
-  lazy val version = "0.1.0-SNAPSHOT"
+  lazy val version = "0.1.0"
   lazy val primaryScalaVersion = "2.11.7"
   lazy val defaultScalacOptions = Seq(
     "-feature",
